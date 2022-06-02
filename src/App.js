8ab7521793acc1a363b7a0099c07100e3843c400
Jean Paul Ari Maldonado <94414304+JeanPaulAri@users.noreply.github.com>
2022-06-02 15:37:27 -0500

    Adding Routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
+﻿
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+} from "react-router-dom";
 import "./Styles/fonts.scss"
 import Home from "./Pages/Home/Home";
+import Login from "./Pages/Login/Login";
+import List from "./Pages/List/List";
+import Single from "./Pages/Single/Single";
+import New from "./Pages/New/New";
+
+
 
 function App() {
   return (
     <div className="App">
-      <Home/>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/">
+                    <Route index element={<Home/>}/>
+                    <Route path="Login" element={<Login/>}/>
+                    <Route path="Users">
+                        <Route index element={<List/>}/>
+                        <Route path=":userID" element={<Single/>}/>
+                        <Route path="New" element={<New/>}/>
+                    </Route>
+                    <Route path="Products">
+                        <Route index element={<List/>}/>
+                        <Route path=":userID" element={<Single/>}/>
+                        <Route path="new" element={<New/>}/>
+                    </Route>
+                </Route>
+            </Routes>
+        </BrowserRouter>
     </div>
   );
 }
