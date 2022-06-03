f201718e1626b939442fc33d4516e8662aa945b6
Jean Paul Ari Maldonado <94414304+JeanPaulAri@users.noreply.github.com>
2022-06-02 21:33:50 -0500

    adding Styles for Navbar 2

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
     return(
         <div className="Navbar">
             <div className="wrapper">
-                <div className="Search">
+                <div className="search">
                     <input type="text" placeholder="Search..."/>
                     <ManageSearchIcon/>
                 </div>
