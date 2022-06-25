47ea6d38572207d613e94c123d39a38f3410143b
Jean Paul Ari Maldonado <94414304+JeanPaulAri@users.noreply.github.com>
2022-06-25 16:26:19 -0500

    adding type props to Widget

--- src/Components/Widget/Widget.jsx
+++ src/Components/Widget/Widget.jsx
@@ -2,29 +2,87 @@ import React from 'react';
 
 import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import AccountBalanceWalletOutlinedIcon from '@mui/icons-material/AccountBalanceWalletOutlined';
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import MonetizationOnOutlinedIcon from '@mui/icons-material/MonetizationOnOutlined';
+
 import './Widget.scss'
-const Widget = () => {
+const Widget = ({ type }) => {
 
+    let data;
+    const amount=100;
+    const diff=20;
+        switch(type){
+            case "user":
+                data={
+                    title:"USERS",
+                    isMoney:false,
+                    link:"See all users",
+                    icon:(
+                        <PersonOutlineOutlinedIcon
+                            className="icon"
+                        />
+                    ),
+                };
+                break;
+            case "order":
+                data={
+                    title:"ORDERS",
+                    isMoney:false,
+                    link:"View all orders",
+                    icon:(
+                        <ShoppingCartOutlinedIcon
+                            className="icon"
+                        />
+                    ),
+                };
+                break;
+            case "earning":
+                data={
+                    title:"EARNINGS",
+                    isMoney:false,
+                    link:"View net earnings",
+                    icon:(
+                        <MonetizationOnOutlinedIcon
+                            className="icon"
+                        />
+                    ),
+                };
+                break;
+            case "balance":
+                data={
+                    title:"BALANCE",
+                    isMoney:false,
+                    link:"See details",
+                    icon:(
+                        <AccountBalanceWalletOutlinedIcon
+                            className="icon"
+                        />
+                    ),
+                };
+                break;
+            default:
+                break;
+        }
     return(
-
         <div className="Widget">
             <div className="left">
-                <span className="tittle">
-                    USERS
+                <span className="title">
+                    {data.title}
                 </span>
                 <span className="counter">
-                    12345678
+                    {data.isMoney && "$"} {amount}
                 </span>
                 <span className="link">
-                    see All users
+                    {data.link}
                 </span>
             </div>
             <div className="right">
                 <div className="percentage positive">
                     <KeyboardArrowUpIcon/>
-                    20  %
+                    {diff} %
                 </div>
-                <PersonOutlineOutlinedIcon className="icon"/>
+                {data.icon}
             </div>
         </div>
     );
